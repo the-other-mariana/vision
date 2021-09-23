b2538e9ee6936ed0f97f90c5dbc57e382b551558
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-09-23 17:32:04 -0500

    shift images done
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- phase-shifting-sinusoidal-code/pattern/pattern/pattern.cpp
+++ phase-shifting-sinusoidal-code/pattern/pattern/pattern.cpp
@@ -15,22 +15,23 @@ int main()
     int w = 1920;
     int h = 1080;
     int pixels = 192;
+
     int waves = w / pixels;
-    float shift = 2.0 * PI / 3.0;
+    float shift = 0.0; // for pure waves, set this to zero
     float factor = (w * 1.0) / (2.0 * PI);
 
     Mat img(h, w, CV_8UC1, Scalar(255));
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             float angle = (j / factor) * waves;
-            uchar value = (255 / 2) + (255 / 2) * cos(angle);
+            uchar value = (255 / 2) + (255 / 2) * cos(angle + shift);
             img.at<uchar>(i, j) = value;
         }
     }
 
     namedWindow("Image", WINDOW_AUTOSIZE);
     imshow("Image", img);
-    imwrite("../test.png", img);
+    imwrite("../zero.png", img);
     waitKey(0);
     
 
