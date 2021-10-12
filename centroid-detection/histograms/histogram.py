945a6033f7cdf6229522395d3a4bbda3b3e6806b
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-10-12 13:18:21 -0500

    blob histogram
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- centroid-detection/histograms/histogram.py
+++ centroid-detection/histograms/histogram.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from math import sqrt
+import numpy as np
 
 def distance(p1, p2):
     return sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
@@ -18,10 +20,37 @@ for i in range(len(lines)):
 
 eps = 10
 pairs = []
+err = []
 for i in range(len(inner_c)):
     c = inner_c[i]
     for j in range(len(outer_c)):
         dist = distance(c, outer_c[j])
         if dist < eps:
             pairs.append([c, outer_c[j]])
-print(pairs)
+            err.append(dist)
+print(err)
+
+intervals = np.linspace(min(err), max(err) * 1.5, 10)
+intervals = list(intervals)
+intervals = [0.0] + intervals
+heights = [0 for i in intervals[1:]]
+xs = list(range(len(heights)))
+
+xlabels = ["[{:.2f}-{:.2f})".format(intervals[i-1], intervals[i]) for i in range(1, len(intervals))]
+
+for i in range(len(intervals) - 1):
+    minVal = intervals[i]
+    maxVal = intervals[i + 1]
+    for e in err:
+        if e >= minVal and e < maxVal:
+            heights[i] += 1
+
+
+print(intervals)
+print(heights, xs)
+
+mpl.rcParams["figure.figsize"] = (12, 4)
+plt.grid(True)
+plt.xticks(xs, labels = xlabels)
+plt.bar(xs,heights, label="Blob Center Error")
+plt.show()
