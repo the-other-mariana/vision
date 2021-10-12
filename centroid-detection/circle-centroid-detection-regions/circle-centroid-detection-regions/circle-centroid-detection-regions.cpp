a0fa897868cd01a85d6c3a41999bd6b26415c1d8
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-10-12 10:12:42 -0500

    cleaned comments
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- centroid-detection/circle-centroid-detection-regions/circle-centroid-detection-regions/circle-centroid-detection-regions.cpp
+++ centroid-detection/circle-centroid-detection-regions/circle-centroid-detection-regions/circle-centroid-detection-regions.cpp
@@ -28,13 +28,15 @@ int main(int argc, char** argv)
     const int h = 864;
     const int hCircles = 8;
     const int vCircles = 6;
+    Scalar colorInner = Scalar(255, 0, 255);
+    Scalar colorOuter = Scalar(255, 0, 0);
 
     int outMax = 7000;
     int outMin = 2000;
     int inMax = 1500;
 
     string pics[3] = { "1.jpg", "2.jpg", "3.jpg" };
-    Mat img = imread("pattern-photos/" + pics[2], 1);
+    Mat img = imread("pattern-photos/" + pics[0], 1);
     Mat centroids(h, w, CV_8UC3, Scalar(255, 255, 255));
 
     Size size(w, h);// aspect ratio 3:4
@@ -67,8 +69,8 @@ int main(int argc, char** argv)
         Point center = Point(k.pt.x, k.pt.y);
         Scalar color = Scalar(250, 250, 250);
         circle(img, center, k.size / 2.0, Scalar(255, 0, 255), 2, 8, 0);
-        drawMarker(img, center, Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
-        drawMarker(centroids, center, Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
+        drawMarker(img, center, colorInner, MARKER_TILTED_CROSS, 10, 2, 8);
+        drawMarker(centroids, center, colorInner, MARKER_TILTED_CROSS, 10, 2, 8);
         circle(img_gray_inv, center, k.size, color, FILLED, 8, 0);
         
     }
@@ -92,8 +94,8 @@ int main(int argc, char** argv)
         KeyPoint k = pts_outer[i];
         Point center = Point(k.pt.x, k.pt.y);
         circle(img, center, k.size / 2.0, Scalar(255, 0, 0), 2, 8, 0);
-        drawMarker(img, center, Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
-        drawMarker(centroids, center, Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
+        drawMarker(img, center, colorOuter, MARKER_CROSS, 10, 2, 8);
+        drawMarker(centroids, center, colorOuter, MARKER_CROSS, 10, 2, 8);
     }
 
 
@@ -113,86 +115,4 @@ int main(int argc, char** argv)
 
     return 0;
 
-    /*
-    Mat img_gray;
-    Mat binary;
-    cvtColor(img, img_gray, COLOR_BGR2GRAY);
-    //Canny(img, img, 50, 150, 3);
-    GaussianBlur(img_gray, img_gray, Size(9, 9), 2, 2);
-    threshold(img_gray, binary, 150, 255, THRESH_BINARY);
-
-    Mat canny_output;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    // detect edges using canny
-    Canny(img_gray, canny_output, 50, 150, 3);
-
-    // find contours
-    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-    // get the moments
-    vector<Moments> outerMoments(contours.size());
-    vector<Moments> innerMoments(contours.size());
-    for (int i = 0; i < contours.size(); i++)
-    {
-        cout << "Area: " << contourArea(contours[i]) << endl;
-        if (contourArea(contours[i]) < outMax && contourArea(contours[i]) > outMin) {
-            outerMoments[i] = moments(contours[i], false);
-            //cout << "Outer: " << contourArea(contours[i]) << endl;
-        }
-        if (contourArea(contours[i]) < inMax) {
-            innerMoments[i] = moments(contours[i], false);
-            //cout << "Inner: " << contourArea(contours[i]) << endl;
-        }
-    }
-
-    // get the centroid of figures.
-    vector<Point2f> oc(outerMoments.size());
-    for (int i = 0; i < outerMoments.size(); i++)
-    {
-        oc[i] = Point2f(outerMoments[i].m10 / outerMoments[i].m00, outerMoments[i].m01 / outerMoments[i].m00);
-    }
-    vector<Point2f> ic(innerMoments.size());
-    for (int i = 0; i < innerMoments.size(); i++)
-    {
-        ic[i] = Point2f(innerMoments[i].m10 / innerMoments[i].m00, innerMoments[i].m01 / innerMoments[i].m00);
-    }
-
-    // draw contours
-    Mat drawing(canny_output.size(), CV_8UC3, Scalar(255, 255, 255));
-    for (int i = 0; i < contours.size(); i++)
-    {
-        if (contourArea(contours[i]) < outMax && contourArea(contours[i]) > outMin) {
-            Scalar color = Scalar(255, 0, 255);
-            drawContours(img, contours, i, color, 2, 8, hierarchy, 0, Point());
-            drawMarker(img, oc[i], Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
-            drawMarker(centroids, oc[i], Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
-        }
-        if (contourArea(contours[i]) < inMax) {
-            Scalar color = Scalar(255, 0, 0);
-            drawContours(img, contours, i, color, 2, 8, hierarchy, 0, Point());
-            drawMarker(img, ic[i], Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
-            drawMarker(centroids, ic[i], Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
-        }
-        
-    }
-
-    
-
-    // timestamp for test img storing
-    string patternImg = getFilename("pattern");
-    string centroidImg = getFilename("centroids");
-
-    namedWindow("Centroids", WINDOW_AUTOSIZE);
-    imshow("Centroids", centroids);
-
-    namedWindow("Image", WINDOW_AUTOSIZE);
-    imshow("Image", img);
-    imwrite(patternImg, img);
-    imwrite(centroidImg, centroids);
-    waitKey(0);
-
-    return 0;
-    */
 }
