65a7476a3a2fb8aa043520665ae0c7b1fb8816ee
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-10-12 10:09:00 -0500

    blob detection using simpleBlobDetector
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- centroid-detection/circle-centroid-detection-regions/circle-centroid-detection-regions/circle-centroid-detection-regions.cpp
+++ centroid-detection/circle-centroid-detection-regions/circle-centroid-detection-regions/circle-centroid-detection-regions.cpp
@@ -33,12 +33,87 @@ int main(int argc, char** argv)
     int outMin = 2000;
     int inMax = 1500;
 
-    string pics[3] = { "center.jpg", "side-tilt.jpg", "front-tilt.jpg" };
+    string pics[3] = { "1.jpg", "2.jpg", "3.jpg" };
     Mat img = imread("pattern-photos/" + pics[2], 1);
     Mat centroids(h, w, CV_8UC3, Scalar(255, 255, 255));
 
     Size size(w, h);// aspect ratio 3:4
     resize(img, img, size);//resize image
+
+    
+    Mat img_gray;
+    Mat img_gray_inv;
+    Mat binary;
+    cvtColor(img, img_gray, COLOR_BGR2GRAY);
+    GaussianBlur(img_gray, img_gray, Size(9, 9), 2, 2);
+    threshold(img_gray, binary, 150, 255, THRESH_BINARY);
+
+    SimpleBlobDetector::Params params1;
+    params1.filterByColor = false;
+    params1.minThreshold = 50;
+    params1.maxThreshold = 255;
+    params1.filterByArea = true;
+    params1.maxArea = 750;
+    params1.filterByCircularity = false;
+    params1.filterByConvexity = false;
+    params1.filterByInertia = false;
+    vector<KeyPoint> pts_inner;
+    Ptr<SimpleBlobDetector> detector1 = SimpleBlobDetector::create(params1);
+
+    bitwise_not(img_gray, img_gray_inv);
+    detector1->detect(img_gray, pts_inner);
+    for (int i = 0; i < pts_inner.size(); i++) {
+        KeyPoint k = pts_inner[i];
+        Point center = Point(k.pt.x, k.pt.y);
+        Scalar color = Scalar(250, 250, 250);
+        circle(img, center, k.size / 2.0, Scalar(255, 0, 255), 2, 8, 0);
+        drawMarker(img, center, Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
+        drawMarker(centroids, center, Scalar(255, 0, 255), MARKER_TILTED_CROSS, 10, 2, 8);
+        circle(img_gray_inv, center, k.size, color, FILLED, 8, 0);
+        
+    }
+    
+    
+    SimpleBlobDetector::Params params2;
+    params2.filterByColor = false;
+    params2.minThreshold = 80;
+    params2.maxThreshold = 255;
+    params2.filterByArea = true;
+    params2.minArea = 750;
+    //params2.maxArea = 1500;
+    params2.filterByCircularity = false;
+    params2.filterByConvexity = false;
+    params2.filterByInertia = false;
+    vector<KeyPoint> pts_outer;
+    Ptr<SimpleBlobDetector> detector2 = SimpleBlobDetector::create(params2);
+
+    detector2->detect(img_gray_inv, pts_outer);
+    for (int i = 0; i < pts_outer.size(); i++) {
+        KeyPoint k = pts_outer[i];
+        Point center = Point(k.pt.x, k.pt.y);
+        circle(img, center, k.size / 2.0, Scalar(255, 0, 0), 2, 8, 0);
+        drawMarker(img, center, Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
+        drawMarker(centroids, center, Scalar(255, 0, 0), MARKER_CROSS, 10, 2, 8);
+    }
+
+
+    // timestamp for test img storing
+    string patternImg = getFilename("pattern");
+    string centroidImg = getFilename("centroids");
+
+    namedWindow("Detection", WINDOW_AUTOSIZE);
+    imshow("Detection", centroids);
+
+    namedWindow("Image", WINDOW_AUTOSIZE);
+    imshow("Image", img);
+
+    imwrite(patternImg, img);
+    imwrite(centroidImg, centroids);
+    waitKey(0);
+
+    return 0;
+
+    /*
     Mat img_gray;
     Mat binary;
     cvtColor(img, img_gray, COLOR_BGR2GRAY);
@@ -119,4 +194,5 @@ int main(int argc, char** argv)
     waitKey(0);
 
     return 0;
+    */
 }
