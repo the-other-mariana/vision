156ee866af24ee5684a1bf827902ef148d90303d
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-10-19 17:31:22 -0500

    unity saves camera frames
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ algorithm-evaluation/renders/Assets/SnapshotCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapshotCamera : MonoBehaviour
+{
+    Camera snapCam;
+    int resWidth = 256;
+    int resHeight = 256; 
+
+    void Awake()
+    {
+        snapCam = GetComponent<Camera>();
+        if (snapCam.targetTexture == null)
+        {
+            snapCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        }
+        else
+        {
+            resWidth = snapCam.targetTexture.width;
+            resHeight = snapCam.targetTexture.height;
+        }
+        snapCam.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+
+    public void TakeSnapshot()
+    {
+        snapCam.gameObject.SetActive(true);
+    }
+
+    void LateUpdate()
+    {
+        if (snapCam.gameObject.activeInHierarchy)
+        {
+            Texture2D snapshot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            snapCam.Render();
+            RenderTexture.active = snapCam.targetTexture;
+            snapshot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            byte[] bytes = snapshot.EncodeToPNG();
+            string fileName = SnapShotName();
+            System.IO.File.WriteAllBytes(fileName, bytes);
+            Debug.Log("Snapshot Taken");
+            snapCam.gameObject.SetActive(false);
+        }
+    }
+
+    string SnapShotName()
+    {
+        return string.Format("{0}/snapshots/snap_{1}x{2}_{3}.png",
+            Application.dataPath,
+            resWidth,
+            resHeight,
+            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+}
