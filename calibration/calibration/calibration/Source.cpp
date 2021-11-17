3f507e9e1857c87d82362715e994f48c4c269fd5
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-11-17 12:09:57 -0600

    copied blob detection to std::vector<KeyPoint> detectNew(const Ptr<FeatureDetector>& blobDetector, Mat image, std::vector<KeyPoint> keypoints) and averaged pts in return vector
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- calibration/calibration/calibration/Source.cpp
+++ calibration/calibration/calibration/Source.cpp
@@ -1,6 +1,7 @@
 #include "precomp.hpp"
 #include "circlesgrid.hpp"
 #include <opencv2/core/utils/logger.defines.hpp>
+#include <math.h>
 //#undef CV_LOG_STRIP_LEVEL
 //#define CV_LOG_STRIP_LEVEL CV_LOG_LEVEL_VERBOSE + 1
 #include <opencv2/core/utils/logger.hpp>
@@ -50,6 +51,92 @@ static int quiet_error(int /*status*/, const char* /*func_name*/,
     return 0;
 }
 
+float euclideanDistance(Point2f p1, Point2f p2) {
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+std::vector<KeyPoint> detectNew(const Ptr<FeatureDetector>& blobDetector, Mat image, std::vector<KeyPoint> keypoints) {
+    const int w = image.cols;
+    const int h = image.rows;
+    const int hCircles = 8;
+    const int vCircles = 6;
+    
+    int limit = 1600;
+    
+    Mat centroids(h, w, CV_8UC3, Scalar(255, 255, 255));
+    Mat img_gray;
+    Mat img_gray_inv;
+    Mat binary;
+    cvtColor(image, img_gray, COLOR_BGR2GRAY);
+    GaussianBlur(img_gray, img_gray, Size(9, 9), 2, 2);
+    threshold(img_gray, binary, 150, 255, THRESH_BINARY);
+
+    // inner circles: blue
+    SimpleBlobDetector::Params params1;
+    params1.filterByColor = false;
+    params1.minThreshold = 50;
+    params1.maxThreshold = 255;
+    params1.filterByArea = true;
+    params1.maxArea = limit;
+    params1.filterByCircularity = false;
+    params1.filterByConvexity = false;
+    params1.filterByInertia = false;
+    std::vector<KeyPoint> pts_inner;
+    Ptr<SimpleBlobDetector> detector1 = SimpleBlobDetector::create(params1);
+    Point2f ip[vCircles * hCircles];
+    Point2f all_centers[vCircles * hCircles * 2];
+
+    bitwise_not(img_gray, img_gray_inv);
+    detector1->detect(img_gray, pts_inner);
+    for (int i = 0; i < pts_inner.size(); i++) {
+        KeyPoint k = pts_inner[i];
+        ip[i] = k.pt;
+        all_centers[i] = k.pt;
+        // comment this on center
+        //circle(img_gray_inv, ip[i], k.size * 0.70, color, FILLED, 8, 0);
+    }
+
+    // outer circles: magenta
+    SimpleBlobDetector::Params params2;
+    params2.filterByColor = false;
+    params2.minThreshold = 80;
+    params2.maxThreshold = 255;
+    params2.filterByArea = true;
+    params2.minArea = limit;
+    //params2.maxArea = 1500;
+    params2.filterByCircularity = false;
+    params2.filterByConvexity = false;
+    params2.filterByInertia = false;
+    std::vector<KeyPoint> pts_outer;
+    Ptr<SimpleBlobDetector> detector2 = SimpleBlobDetector::create(params2);
+    Point2f op[vCircles * hCircles];
+
+    detector2->detect(img_gray_inv, pts_outer);
+    for (int i = 0; i < pts_outer.size(); i++) {
+        KeyPoint k = pts_outer[i];
+        op[i] = k.pt;
+        all_centers[(vCircles* hCircles) + i] = k.pt;
+    }
+
+    int eps = 10;
+    for (int i = 0; i < (vCircles * hCircles * 2); i++) {
+        Point2f c = all_centers[i];
+        for (int j = 0; j < (vCircles * hCircles * 2); j++) {
+            if (i == j) {
+                continue;
+            }
+            float dist = euclideanDistance(c, all_centers[i]);
+            if (dist < eps) {
+                float avgX = (c.x + all_centers[i].x) / 2.0;
+                float avgY = (c.y + all_centers[i].y) / 2.0;
+                Point2f avg_center = Point2f(avgX, avgY);
+                keypoints.push_back(KeyPoint(avg_center, 1.f));
+            }
+        }
+    }
+    return keypoints;
+}
+
 bool findCirclesGridNew(InputArray _image, Size patternSize,
     OutputArray _centers, int flags, const Ptr<FeatureDetector>& blobDetector,
     const CirclesGridFinderParameters& parameters_)
@@ -66,8 +153,10 @@ bool findCirclesGridNew(InputArray _image, Size patternSize,
     std::vector<Point2f> centers;
 
     std::vector<KeyPoint> keypoints;
-    // here
-    blobDetector->detect(image, keypoints);
+    // original line
+    //blobDetector->detect(image, keypoints);
+    // added line
+    keypoints = detectNew(blobDetector, image, keypoints);
     std::vector<Point2f> points;
     for (size_t i = 0; i < keypoints.size(); i++)
     {
